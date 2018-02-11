424233ae26de28f61b1d797b2526c6fd743d6323
brunosiillva <bruno.siilva@hotmail.com>
2018-02-11 16:16:05 -0200

    Iniciando routes com AngularJs

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('desafio',[]);
+var myApp = angular.module('desafio',['ngRoute']);
 
 myApp.controller('listaLivros', ['$scope', '$http', function($scope, $http){
 
@@ -8,7 +8,8 @@ myApp.controller('listaLivros', ['$scope', '$http', function($scope, $http){
      url: 'https://www.googleapis.com/books/v1/volumes?q=star+wars'
   }).then(function (success){
     $scope.books = success.data.items;
-    console.log(success.data.items, 'Informacoes pegas com sucesso');
+    // console.log(success.data.items, 'Informacoes pegas com sucesso');
+    console.log('Informacoes pegas com sucesso');
   },function (error){
     console.log(error, 'Erro ao pegar as informacoes');
   });
