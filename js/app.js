ba4866d3a3746b409414039acf905ae3119834b8
brunosiillva <bruno.siilva@hotmail.com>
2018-02-11 14:18:19 -0200

    Pegando as informações da url com angularjs 1.6+

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 var myApp = angular.module('desafio',[]);
+
+myApp.controller('listaLivros', ['$scope', '$http', function($scope, $http){
+
+  //Pega infos da lista completa
+  $http({
+     method: 'GET',
+     url: 'https://www.googleapis.com/books/v1/volumes?q=star+wars'
+  }).then(function (success){
+    $scope.data = success;
+    console.log(success);
+  },function (error){
+
+  });
+  //Fim lista completa
+
+}]);
